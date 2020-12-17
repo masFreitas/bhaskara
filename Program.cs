d135a3cc3df7c43a3f8629bab6b61d018c6f1636
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-16 21:42:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExercicioTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULAR BHASKARA!!!");
+            double a, b, c, delta, x1, x2, raiz;
+
+            Console.WriteLine("Informe um numero inteiro para 'a':");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe um numero inteiro para 'b':");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe um numero inteiro para 'c':");
+            c = double.Parse(Console.ReadLine());
+
+            delta = Math.Pow(b,2) - (4*a*c);
+
+            Console.WriteLine("delta é {0}", delta);
+            raiz = Math.Sqrt(delta);
+            
+            x1 = (-(b) + raiz) / (2*a);
+            x2 = (-(b) - raiz) / (2*a);
+            Console.WriteLine("x' é {0}", x1);
+            Console.WriteLine("x''é {0}", x2);
+
+        }
+    }
+}
